09a2ef5815676f9e799d807c18bd51665b309cd0
Vinoth-Devopss <vinoth03.tnhsp@gmail.com>
2024-06-12 17:21:36 +0530

    Update IAM.tf

--- Modules/IAM/IAM.tf
+++ Modules/IAM/IAM.tf
@@ -1,7 +1,27 @@
-/*
+
 resource "aws_iam_user" "administrator" {
-  name = "Administrator"
+  name = "administrator"
+  path = "/system/"
+
+  tags = {
+    tag-key = "tag-value"
+  }
 }
 
+resource "aws_iam_access_key" "administrator" {
+  user = aws_iam_user.administrator.name
+}
 
-*/
+data "aws_iam_policy_document" "administrator_ro" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "administrator_ro" {
+  name   = "administrator_ro"
+  user   = aws_iam_user.administrator.name
+  policy = data.aws_iam_policy_document.administrator_ro.json
+}
