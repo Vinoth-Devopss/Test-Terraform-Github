ab38a1078c8d4a082cab86d45cbfdd4d99a34892
Vinoth-Devopss <vinoth03.tnhsp@gmail.com>
2024-06-12 15:27:05 +0530

    Update dynamodb.tf

--- Modules/DYNAMODB/dynamodb.tf
+++ Modules/DYNAMODB/dynamodb.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "Dev-vinoth-terraform-remote-state-lock"
+  name           = "dev-vinoth-terraform-remote-state-lock"
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "LockID"
