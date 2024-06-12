4a64b5782542c20bd8647d5d931ab1ed86458210
Vinoth-Devopss <vinoth03.tnhsp@gmail.com>
2024-06-12 19:56:54 +0530

    Create ec2.tf

---
+++ Modules/EC2/ec2.tf
@@ -0,0 +1,30 @@
+#Create a new Vinoth-TF prod API -1 EC2 launch configuration..
+
+resource "aws_instance" "prod-api-1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.prod-api_key_name
+  vpc_security_group_ids = [var.security_group_id]
+  subnet_id     = var.public-subnet-1_id
+  associate_public_ip_address = true
+  disable_api_termination = true
+  
+  tags = {
+Name = "Dev-vinoth-TF"
+}
+
+root_block_device {
+    delete_on_termination = "true"
+    encrypted             = "false"
+    iops                  = "3000"
+    volume_size           = "20"
+    volume_type           = "gp3"
+  }
+
+connection {
+    type = "ssh"
+    user = "administrator"
+    password = var.administrator_password
+    private_key = file("${path.module}/prod-administrator.pem")
+    host = self.public_ip    
+    } 
