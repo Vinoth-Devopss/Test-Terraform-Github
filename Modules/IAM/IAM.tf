0dbf52d97327e2fb8b5840791bfc5f68d8cce0bb
Vinoth-Devopss <vinoth03.tnhsp@gmail.com>
2024-06-12 17:06:18 +0530

    Update IAM.tf

--- Modules/IAM/IAM.tf
+++ Modules/IAM/IAM.tf
@@ -4,9 +4,6 @@ resource "aws_iam_user" "administrator" {
 
 data "aws_iam_policy" "administrator_access" {
   name = "AdministratorAccess"
-}
-
-resource "aws_iam_group_policy_attachment" "administrators" {
   user      = aws_iam_user.administrator.name
   policy_arn = data.aws_iam_policy.administrator_access.arn
 }
