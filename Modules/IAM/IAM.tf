5cc929430901f5d6b3c3ade65b7ea7d2a2ce59cc
Vinoth-Devopss <vinoth03.tnhsp@gmail.com>
2024-06-12 17:02:43 +0530

    Update IAM.tf

--- Modules/IAM/IAM.tf
+++ Modules/IAM/IAM.tf
@@ -5,3 +5,18 @@ resource "aws_iam_user" "administrator" {
 data "aws_iam_policy" "administrator_access" {
   name = "AdministratorAccess"
 }
+
+resource "aws_iam_group_policy_attachment" "administrators" {
+  user      = aws_iam_user.administrator.name
+  policy_arn = data.aws_iam_policy.administrator_access.arn
+}
+
+resource "aws_iam_user_login_profile" "administrator" {
+  user                    = aws_iam_user.administrator.name
+  password_reset_required = true
+}
+
+output "password" {
+  value     = aws_iam_user_login_profile.administrator.password
+  sensitive = true
+}
